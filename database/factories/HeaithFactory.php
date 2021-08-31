62b680460b187ddca1573c700d00299b97b2f4ec
Olga Shmakova <olg4983@gmail.com>
2021-09-01 00:18:56 +0300

    create db

---
+++ database/factories/HeaithFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Heaith;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HeaithFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Heaith::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_08_31_205606_create_evolutions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvolutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evolutions', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('age_month');
+            $table->tinyInteger('growth');
+            $table->tinyInteger('weight');
+
+            $table->unsignedBigInteger('children_id');
+
+            $table->foreign('children_id')
+                ->references('id')
+                ->on('children')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evolutions');
+    }
+}
