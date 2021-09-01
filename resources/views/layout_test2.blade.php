90e9fcc40aa117f55905aa0a75119a7a79be29fb
Sergey Kiselev <kiselev.itstep@gmail.com>
2021-09-01 23:09:46 +0300

    upgrade design navbar

--- resources/views/layout_test2.blade.php
+++ resources/views/layout_test2.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css'><link rel="stylesheet" href="test_style2.css">
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
 <nav class="navbar navbar-expand-custom navbar-mainbg">
-    <a class="navbar-brand navbar-logo" href="#">Navbar</a>
+    <a class="navbar-brand navbar-logo" href="#">Oh baby 2.0</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars text-white"></i>
     </button>
@@ -19,16 +20,16 @@
         <ul class="navbar-nav ml-auto">
             <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
             <li class="nav-item">
-                <a class="nav-link" href="/test2"><i class="fas fa-tachometer-alt"></i>Home</a>
+                <a class="nav-link" href="/test2"><i class="fas fa-home"></i>Home</a>
             </li>
             <li class="nav-item active">
                 <a class="nav-link" href="/docs_test2"><i class="far fa-copy"></i>Documents</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/health_test2"><i class="far fa-clone"></i>Health</a>
+                <a class="nav-link" href="/health_test2"><i class="far fa-copy"></i>Health</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/growth_test2"><i class="far fa-calendar-alt"></i>Growth</a>
+                <a class="nav-link" href="/growth_test2"><i class="fa fa-copy"></i>Growth</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#"><i class="far fa-chart-bar"></i>Settings</a>
@@ -49,7 +50,8 @@
 </div>
 
 <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
-<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script><script  src="test2.js"></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+<script src="test2.js"></script>
 
 </body>
 </html>
