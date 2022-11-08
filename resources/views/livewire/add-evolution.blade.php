6cc7bb7a54d9b1bde294ea3af13b4322ad2ca065
Sergey Kiselev <kiselev.itstep@gmail.com>
2022-11-08 17:25:33 +0200

    editing add evolution

--- resources/views/livewire/add-evolution.blade.php
+++ resources/views/livewire/add-evolution.blade.php
@@ -1,57 +1,102 @@
-<div class="flex">
-    <div>
-        <div class="flex items-center ">
-
-            <svg class="h-10 w-10 text-green-400
-{{--            hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200--}}
-                hover:text-green-200 hover:cursor-pointer
-                "
-                 fill="none" stroke-linecap="round"
-                 stroke-linejoin="round" stroke-width="2" stroke="currentColor"
-                 viewBox="0 0 24 24">
-                <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
-
-            <!-- Select  -->
-            <a id="" type="button" href="#"
-               class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
-               wire:click="">
-                {{'Month'}}
-            </a>
+<div class="flex flex-wrap">
+    <div class="flex flex-wrap lg:w-1/2 sm:w-full">
+        <div class="mr-4">
+            {{'Возраст:'}}
         </div>
-        <div class="flex items-center">
+        <div>
+            <div class="flex items-center ">
+                <div class="flex items-center w-1/3">
+                    <svg class="h-10 w-10 text-green-400
+        {{--            hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200--}}
+                        hover:text-green-200 hover:cursor-pointer
+                        "
+                         fill="none" stroke-linecap="round"
+                         stroke-linejoin="round" stroke-width="2" stroke="currentColor"
+                         viewBox="0 0 24 24">
+                        <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
 
-            <svg class="h-10 w-10 text-gray-300
-                hover:text-green-200 hover:cursor-pointer
-                "
-                 fill="none" stroke-linecap="round"
-                 stroke-linejoin="round" stroke-width="2" stroke="currentColor"
-                 viewBox="0 0 24 24">
-                <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
+                    <!-- Select  -->
+                    <a id="" href="#"
+                       class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
+                       wire:click="">
+                        {{'up to one year'}}
+                    </a>
+                </div>
 
-            <!-- Select  -->
-            <a id="" href="#"
-               class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
-               wire:click="">
-                {{'Year'}}
-            </a>
+                <x-jet-input id="growth" type="text" class="mt-1 block" wire:model.defer="growth" autocomplete="growth" />
+                <x-jet-input-error for="growth" class="mt-2" />
+            </div>
+            <div class="flex items-center">
+                <div class="flex items-center w-1/3">
+                    <svg class="h-10 w-10 text-gray-300
+                        hover:text-green-200 hover:cursor-pointer
+                        "
+                         fill="none" stroke-linecap="round"
+                         stroke-linejoin="round" stroke-width="2" stroke="currentColor"
+                         viewBox="0 0 24 24">
+                        <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+
+                    <!-- Select  -->
+                    <a id="" href="#"
+                       class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
+                       wire:click="">
+                        {{'after one year'}}
+                    </a>
+                </div>
+
+                <x-jet-input id="growth" type="text" class="mt-1 block" wire:model.defer="growth" autocomplete="growth" />
+                <x-jet-input-error for="growth" class="mt-2" />
+            </div>
         </div>
     </div>
-
-    <div class="flex">
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="growth" value="{{ __('Growth') }}" />
-            <x-jet-input id="growth" type="text" class="mt-1 block" wire:model.defer="growth" autocomplete="growth" />
-            <x-jet-input-error for="growth" class="mt-2" />
+    <div class="flex flex-wrap lg:w-1/2 sm:w-full">
+        <div class="mr-4">
+            {{'Параметры:'}}
         </div>
+        <div>
+            <div class="flex items-center ">
+                <div class="flex items-center mr-4">
+                    <x-jet-label for="growth" value="{{ __('Growth') }}" />
+                </div>
 
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="weight" value="{{ __('Weight') }}" />
-            <x-jet-input id="weight" type="text" class="mt-1 block" wire:model.defer="weight" autocomplete="weight" />
-            <x-jet-input-error for="weight" class="mt-2" />
+                <x-jet-input id="growth" type="text" class="mt-1 block" wire:model.defer="growth" autocomplete="growth" />
+                <x-jet-input-error for="growth" class="mt-2" />
+            </div>
+            <div class="flex items-center">
+                <div class="flex items-center mr-4">
+                    <x-jet-label for="weight" value="{{ __('Weight') }}" />
+                </div>
+
+                <x-jet-input id="growth" type="text" class="mt-1 block" wire:model.defer="growth" autocomplete="growth" />
+                <x-jet-input-error for="growth" class="mt-2" />
+            </div>
         </div>
     </div>
+{{--    <div class="flex flex-wrap lg:w-1/2 sm:w-full">--}}
+{{--        <div class="mr-4">--}}
+{{--            {{'Параметры:'}}--}}
+{{--        </div>--}}
+{{--        <div class="flex items-center">--}}
+{{--            <div class="flex items-center ">--}}
+{{--                <div class="flex items-center w-1/3">--}}
+{{--                    <x-jet-label for="growth" value="{{ __('Growth') }}" />--}}
+{{--                </div>--}}
+
+{{--                <x-jet-input id="growth" type="text" class="mt-1 block sm:w-full" wire:model.defer="growth" autocomplete="growth" />--}}
+{{--                <x-jet-input-error for="growth" class="mt-2" />--}}
+{{--            </div>--}}
+{{--            <div class="flex items-center">--}}
+{{--                <div class="flex items-center w-1/3">--}}
+{{--                    <x-jet-label for="weight" value="{{ __('Weight') }}" />--}}
+{{--                </div>--}}
+
+{{--                <x-jet-input id="weight" type="text" class="mt-1 block" wire:model.defer="weight" autocomplete="weight" />--}}
+{{--                <x-jet-input-error for="weight" class="mt-2" />--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div>
 
 <div class="flex">
