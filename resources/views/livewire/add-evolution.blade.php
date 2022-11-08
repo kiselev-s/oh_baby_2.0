5d0164ac12460878ae08963db02a9768812b9360
Sergey Kiselev <kiselev.itstep@gmail.com>
2022-11-08 22:46:45 +0200

    fix add evoluion

--- resources/views/livewire/add-evolution.blade.php
+++ resources/views/livewire/add-evolution.blade.php
@@ -18,8 +18,7 @@
 
                     <!-- Select  -->
                     <a id="" href="#"
-                       class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
-                       wire:click="">
+                       class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out">
                         {{'up to one year'}}
                     </a>
                 </div>
@@ -40,8 +39,7 @@
 
                     <!-- Select  -->
                     <a id="" href="#"
-                       class="w-full ml-3 inline-block px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out"
-                       wire:click="">
+                       class="w-full ml-3 px-1 py-2 bg-transparent text-gray-700 font-medium text-sm leading-tight transition duration-150 ease-in-out">
                         {{'after one year'}}
                     </a>
                 </div>
@@ -74,32 +72,9 @@
             </div>
         </div>
     </div>
-{{--    <div class="flex flex-wrap lg:w-1/2 sm:w-full">--}}
-{{--        <div class="mr-4">--}}
-{{--            {{'Параметры:'}}--}}
-{{--        </div>--}}
-{{--        <div class="flex items-center">--}}
-{{--            <div class="flex items-center ">--}}
-{{--                <div class="flex items-center w-1/3">--}}
-{{--                    <x-jet-label for="growth" value="{{ __('Growth') }}" />--}}
-{{--                </div>--}}
-
-{{--                <x-jet-input id="growth" type="text" class="mt-1 block sm:w-full" wire:model.defer="growth" autocomplete="growth" />--}}
-{{--                <x-jet-input-error for="growth" class="mt-2" />--}}
-{{--            </div>--}}
-{{--            <div class="flex items-center">--}}
-{{--                <div class="flex items-center w-1/3">--}}
-{{--                    <x-jet-label for="weight" value="{{ __('Weight') }}" />--}}
-{{--                </div>--}}
-
-{{--                <x-jet-input id="weight" type="text" class="mt-1 block" wire:model.defer="weight" autocomplete="weight" />--}}
-{{--                <x-jet-input-error for="weight" class="mt-2" />--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 </div>
 
-<div class="flex">
+<div class="flex justify-center">
     <div class="mr-4">
         <x-jet-button wire:click="store()" wire:loading.attr="disabled">
             {{ __('Save')  }}
